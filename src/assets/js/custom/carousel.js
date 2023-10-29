d360deaad67fae06913c011bac719a41d4b9f421
Okta Pratama <oktaiwp849@gmail.com>
2023-10-29 19:23:29 +0700

    new update

--- src/assets/js/custom/carousel.js
+++ src/assets/js/custom/carousel.js
@@ -4,7 +4,7 @@
         "image": "project1.jpg",
         "name": "Sistem Manajemen Keuangan",
         "description": "I led the development of the Financial Management System project, aimed at helping companies track and manage their finances. We used technologies such as JavaScript, React, and Node.js. You can find the project on GitHub.",
-        "technologies": ["JavaScript", "React", "Node.js"],
+        "technologies": ["JavaScript", "React", "Node js"],
         "liveServerLink": "https://www.example.com/live-server1",
         "githubRepo": "https://github.com/username/financial-management"
       },
@@ -20,24 +20,24 @@
         "image": "project3.jpg",
         "name": "Aplikasi Mobile E-commerce",
         "description": "This project involved developing a Mobile E-commerce Application that allows users to shop for products online. I was responsible for developing the front-end using React Native. The project leverages modern technologies like React Native, Redux, and Firebase.",
-        "technologies": ["React Native", "Redux", "Firebase"],
+        "technologies": ["PHP", "Laravel", "SASS"],
         "liveServerLink": "https://www.example.com/live-server3",
         "githubRepo": "https://github.com/username/e-commerce-app"
       }
     ],
     "certificates": [{
-        "image": "certificate1_image.jpg",
-        "title": "Sertifikat 1",
+        "image": "assets/media/images/Photograph.jpg",
+        "title": "Photography & VideoGraphy",
         "description": "This is the first certificate."
       },
       {
-        "image": "certificate2_image.jpg",
-        "title": "Sertifikat 2",
+        "image": "assets/media/images/Journalism.jpg",
+        "title": "Journalism & Reportage",
         "description": "This is the second certificate."
       },
       {
-        "image": "certificate3_image.jpg",
-        "title": "Sertifikat 3",
+        "image": "assets/media/images/digitalmagz.jpg",
+        "title": "Digital Magazine Application",
         "description": "This is the third certificate."
       }
     ]
@@ -57,9 +57,21 @@
       var projectDiv = document.createElement("div");
       projectDiv.className = "item";
       projectDiv.innerHTML = 
-      "<img src='" +project.image+"'>" +
-      "<h3>" + project.name + "</h3>" +
-      "<p>" + project.description + "</p>";
+        "<img src='" + project.image + "'>" +
+        "<h5>" + project.name + "</h5>" +
+        "<p>" + project.description + "</p>";
+  
+      var technologiesDiv = document.createElement("div");
+      technologiesDiv.className = "technologies";
+      
+      project.technologies.forEach(function (technology) {
+        var technologyAnchor = document.createElement("a");
+        technologyAnchor.className = technology;
+        technologyAnchor.innerText = technology; 
+        technologiesDiv.appendChild(technologyAnchor); 
+      });
+  
+      projectDiv.appendChild(technologiesDiv);
       projectItem.appendChild(projectDiv);
     });
   }
@@ -70,7 +82,7 @@
       certificateDiv.className = "item";
       certificateDiv.innerHTML = 
         "<img src='" +certificate.image+"'>"  +
-        "<h3>" + certificate.title + "</h3>" +
+        "<h5>" + certificate.title + "</h5>" +
         "<p>" + certificate.description + "</p>";
         certificateItem.appendChild(certificateDiv);
     });
@@ -78,9 +90,6 @@
 
   fillProjectList();
   fillCertificatesList();
-
-
-
 
   projectButton.addEventListener("click", () => {
     projectList.classList.add("list-active");
